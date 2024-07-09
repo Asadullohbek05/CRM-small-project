c2ad9c05e6854f4046874872c1052795ff02a333
Asadullohbek05 <ayubovasadbek33@gmail.com>
2024-07-09 11:51:25 +0500

    modal added

--- src/components/ProductsHeader.jsx
+++ src/components/ProductsHeader.jsx
@@ -9,6 +9,42 @@ const ProductsHeader = ({ search, setSearch, category, setCategory, sorting, set
                 </div>
                 <div className="flex-none w-[95%] md:w-auto flex flex-col md:flex-row items-center">
                     <select
+                        className="select select-bordered w-full hidden md:block md:w-auto mb-2 md:mb-0 md:me-3"
+                        value={category}
+                        onChange={(e) => setCategory(e.target.value)}
+                    >
+                        <option value="all">All</option>
+                        <option value="Fruits">Fruits</option>
+                        <option value="Vegetables">Vegetables</option>
+                        <option value="Dairy">Dairy</option>
+                        <option value="Beverages">Beverages</option>
+                        <option value="Bakery">Bakery</option>
+                    </select>
+                    <div className="flex w-full items-center gap-3 md:w-auto mb-2 md:mb-0 md:me-2">
+                        <input
+                            value={search}
+                            onChange={(e) => setSearch(e.target.value.trim().toLowerCase())}
+                            type="text"
+                            placeholder="Search product"
+                            className="input input-bordered w-full md:w-auto"
+                        />
+                        <button onClick={() => document.getElementById('my_modal_2').showModal()} className='btn btn-primary md:hidden text-white'>Filter</button>
+
+                    </div>
+                    <select
+                        className="select select-bordered w-full md:w-[150px] hidden md:block"
+                        value={sorting}
+                        onChange={(e) => setSorting(e.target.value)}
+                    >
+                        <option value="Increase">Increase</option>
+                        <option value="Decrease">Decrease</option>
+                    </select>
+                </div>
+            </div>
+            <dialog id="my_modal_2" className="modal">
+                <div className="modal-box">
+                    <h1 className='text-center mb-3'>Filtering by Category </h1>
+                    <select
                         className="select select-bordered w-full md:w-auto mb-2 md:mb-0 md:me-3"
                         value={category}
                         onChange={(e) => setCategory(e.target.value)}
@@ -20,15 +56,7 @@ const ProductsHeader = ({ search, setSearch, category, setCategory, sorting, set
                         <option value="Beverages">Beverages</option>
                         <option value="Bakery">Bakery</option>
                     </select>
-                    <div className="form-control w-full md:w-auto mb-2 md:mb-0 md:me-2">
-                        <input
-                            value={search}
-                            onChange={(e) => setSearch(e.target.value.trim().toLowerCase())}
-                            type="text"
-                            placeholder="Search product"
-                            className="input input-bordered w-full md:w-auto"
-                        />
-                    </div>
+                    <h1 className='text-center mb-3'>Filtering by Price </h1>
                     <select
                         className="select select-bordered w-full md:w-[150px]"
                         value={sorting}
@@ -38,7 +66,10 @@ const ProductsHeader = ({ search, setSearch, category, setCategory, sorting, set
                         <option value="Decrease">Decrease</option>
                     </select>
                 </div>
-            </div>
+                <form method="dialog" className="modal-backdrop">
+                    <button>close</button>
+                </form>
+            </dialog>
         </div>
     );
 };
